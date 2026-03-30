03bd1023ed5930d5ec76590cb93e2f3d841fa53e
Jay Patel <pateljay1032003@gmail.com>
2026-03-29 18:40:05 -0600

    adding env file

--- Code/backend/server.js
+++ Code/backend/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS (safe defaults for JWT in Authorization header)
 app.use(
   cors({
-    origin: true,
+    origin: "http://54.85.60.202",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
